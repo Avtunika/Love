d923d70e43f79142d0b4d9a220b3ca8a2824c4cf
Avtunika <116015010+Avtunika@users.noreply.github.com>
2022-11-11 22:54:37 +0400

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const b = document.querySelector('button.button--no')
 b.addEventListener("mouseover", moveHover)
 
 function moveHover(){
-    const i=Math.floor(Math.random()*500)+5 ;
-    const j=Math.floor(Math.random()*500)+5 ;
+    const i=Math.floor(Math.random()*500)+1 ;
+    const j=Math.floor(Math.random()*500)+1 ;
     
 
      b.style.bottom=i+"px"
